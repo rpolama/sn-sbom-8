8cf120b92e1f707ccf632ab8ff07c6f473a6e3ec
anushkumar.mannam <anushkumar.mannam@servicenow.com>
2023-02-22 19:02:56 +0530

    Project

---
+++ src/main/java/com/servicenow/snsbom8/SnSbom8Application.java
@@ -0,0 +1,13 @@
+package com.servicenow.snsbom8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnSbom8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnSbom8Application.class, args);
+	}
+
+}
